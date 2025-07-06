84a90f78a03fdfc090fa787f1b3673a245d9a417
Aditya Vardhan <inintr00329@DN9R44MYVK.local>
2025-07-06 17:37:57 +0530

    full code

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+import express  from "express"
+import cors from 'cors'
+import { connectDB } from "./config/db.js"
+import userRouter from "./routes/userRoute.js"
+import foodRouter from "./routes/foodRoute.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
+import orderRouter from "./routes/orderRoute.js"
+import "./config/passport.js" 
+import passport from "passport"
+import session from "cookie-session"
+
+// app config
+const app = express()
+const port = 4000
+
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+app.use(
+  session({
+    name: "session",
+    keys: ["secretKey"],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// OAuth2 routes
+app.get("/oauth2/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+
+app.get(
+  "/oauth2/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    // redirect to React with token and email
+    const { token, email } = req.user;
+    res.redirect(`${process.env.CLIENT_URL}/oauth-success?token=${token}&email=${email}`);
+  }
+);
+
+// db connection
+connectDB()
+
+// api endpoints
+app.use("/api/user", userRouter)
+app.use("/api/food", foodRouter)
+app.use("/images",express.static('uploads'))
+app.use("/api/cart", cartRouter)
+app.use("/api/order",orderRouter)
+
+app.get("/", (req, res) => {
+    res.send("API Working")
+  });
+
+app.listen(port, () => console.log(`Server started on http://localhost:${port}`))
