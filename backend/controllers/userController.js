97fc8493e029e8991f22f8d6c7fac75958697bbb
Aditya Vardhan <inintr00329@DN9R44MYVK.local>
2025-07-12 15:42:33 +0530

    email verification added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,9 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import validator from "validator";
 import userModel from "../models/userModel.js";
+import crypto from "crypto";
+import otpModel from "../models/otpModel.js";
+import nodemailer from "nodemailer";
 
 //create token
 const createToken = (id) => {
@@ -70,4 +73,60 @@ const registerUser = async (req, res) => {
   }
 };
 
-export { loginUser, registerUser };
+
+const verifyEmail = async (req,res)=>{
+   const {email}=req.body;
+
+   const existingUser = await userModel.findOne({email});
+    if (existingUser) {
+       return res.json({ success: false, message: "User already exists" });
+    }
+   const otp = crypto.randomInt(100000, 999999).toString();
+
+   const expiresAt = new Date(Date.now() + 1 * 60 * 1000); 
+   
+    await otpModel.findOneAndUpdate(
+    { email },
+    { otp, expiresAt },
+    { upsert: true, new: true }
+  );
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"MyApp" <${process.env.EMAIL_USER}>`,
+    to: email,
+    subject: "Your OTP Code",
+    html: `<h2>Your OTP: ${otp}</h2><p>It will expire in 5 minutes.</p>`,
+  });
+
+   res.json({ success: true, message: "OTP sent to email" });
+
+}
+
+const verifyOtp = async (req,res)=>{
+     const {email,otp}= req.body;
+     const otpData = await otpModel.findOne({email});
+     console.log(otpData)
+     if(!otpData){
+       return res.json({ success:false, message: "Invalid or expired OTP"})
+     }
+
+     if (otpData.otp !== otp || otpData.expiresAt < new Date()) {
+        return res.status(400).json({ success: false, message: "Invalid or expired OTP" });
+    }
+
+    await otpModel.deleteOne({ email });
+
+    return res.json({ success: true, message: "OTP verified successfully" });
+
+
+}
+
+export { loginUser, registerUser, verifyEmail, verifyOtp };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { loginUser,registerUser } from '../controllers/userController.js';
+import { verifyEmail, verifyOtp } from '../controllers/userController.js';
+
 const userRouter = express.Router();
 
 userRouter.post("/register",registerUser);
 userRouter.post("/login",loginUser);
+userRouter.post("/verify-email",verifyEmail);
+userRouter.post("/verify-otp", verifyOtp);
 
 export default userRouter;
